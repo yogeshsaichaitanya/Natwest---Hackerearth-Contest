a62324782a13a35fd41fde13100ed261652a4722
yogeshsaichaitanya <72433671+yogeshsaichaitanya@users.noreply.github.com>
2022-04-10 23:20:43 +0530

    Add files via upload

---
+++ secure_pseudo_queue/src/main/java/com/natwest/secure_pseudo_queue/SecurePseudoQueueApplication.java
@@ -0,0 +1,13 @@
+package com.natwest.secure_pseudo_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePseudoQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePseudoQueueApplication.class, args);
+	}
+
+}
---
+++ secure_pseudo_queue/src/test/java/com/natwest/secure_pseudo_queue/SecurePseudoQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natwest.secure_pseudo_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePseudoQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
